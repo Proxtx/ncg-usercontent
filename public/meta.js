41504085f1d2829e486e9af1e29f4bbf58e022fb
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-01-28 22:06:37 +0100

    Retry auth if failure

--- public/meta.js
+++ public/meta.js
@@ -2,11 +2,18 @@ export const auth = async (username, password) => {
   let data = new FormData();
   data.append("username", username);
   data.append("password", password);
-  let res = await (
+  let res;
+  do {
+    try{
+    res = await (
     await fetch("https://abi.vtsem.de/auth", {
       body: data,
       method: "POST",
     })
   ).json();
+      }
+    catch {}
+    }
+  while(!res)
   return res.success;
 };
