6cc78227ae62177a25be08d050b9f17f87aefd45
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-02-03 15:28:17 +0100

    added a login redirect to non-login pages

---
+++ private/render/autoRedirect.js
@@ -0,0 +1,10 @@
+import { auth } from "../../public/meta.js";
+
+export const server = async (document, options) => {
+  if (
+    !(await auth(options.req.cookies.username, options.req.cookies.password))
+  ) {
+    options.res.redirect("/");
+    return;
+  }
+};
