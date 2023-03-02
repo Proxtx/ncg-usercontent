19ed391811e85587111cecfa8817d3735ab09dd5
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-03-02 16:43:26 +0100

    ohh i completely broke the login

--- static/login/main.js
+++ static/login/main.js
@@ -14,16 +14,17 @@ if (
   cookie.username &&
   cookie.password &&
   (await meta.auth(cookie.username, cookie.password))
-)
-  //location.pathname = "/";
+) {
+}
+//location.pathname = "/";
 
-  loginButton.addEventListener("click", async () => {
-    await new Promise((r) => setTimeout(r, 500));
-    cookie.username = username.component.value;
-    cookie.password = password.component.value;
-    if (!(await meta.auth(username.component.value, password.component.value)))
-      alert("Benutzername oder Passwort ist falsch");
-    else {
-      location.pathname = "/";
-    }
-  });
+loginButton.addEventListener("click", async () => {
+  await new Promise((r) => setTimeout(r, 500));
+  cookie.username = username.component.value;
+  cookie.password = password.component.value;
+  if (!(await meta.auth(username.component.value, password.component.value)))
+    alert("Benutzername oder Passwort ist falsch");
+  else {
+    location.pathname = "/";
+  }
+});
