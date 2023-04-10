542077dadd150f7f3b81b4735386867bfd9a85b5
Proxtx <39201896+Proxtx@users.noreply.github.com>
2023-04-11 01:37:04 +0200

    improve access checking

--- public/content.js
+++ public/content.js
@@ -1,5 +1,6 @@
 import { auth } from "./meta.js";
 import { getUserFile } from "../private/content.js";
+import config from "@proxtx/config";
 
 export const addContent = async (
   username,
@@ -18,11 +19,23 @@ export const addContent = async (
 };
 
 export const getContent = async (username, password, contentUsername) => {
-  if (!(await auth(username, password)) || username != contentUsername) return;
+  if (!(await auth(username, password))) return;
+  if (!(await hasAccess(username, password, contentUsername))) return;
   return await (await getUserFile(contentUsername)).getContent();
 };
 
 export const deleteContent = async (username, password, index) => {
   if (!(await auth(username, password))) return;
   await (await getUserFile(username)).deleteContent(index);
+};
+
+export const hasAccess = async (username, password, contentUsername) => {
+  if (!(await auth(username, password))) return false;
+  if (
+    (config.access[contentUsername] &&
+      config.access[contentUsername].includes(username)) ||
+    contentUsername == username
+  )
+    return true;
+  return false;
 };
